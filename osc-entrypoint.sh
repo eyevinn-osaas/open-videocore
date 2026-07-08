0280dc52b44078427c376cea1138159ebb4d646a
Jonas Birmé <jonas.birme@gmail.com>
2026-07-08 10:22:08 +0200

    feat: add OSC entrypoint to set PUBLIC_BASE_URL from OSC_HOSTNAME

---
+++ osc-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Map OSC_HOSTNAME to PUBLIC_BASE_URL so Encore profile loading works correctly
+if [ -n "$OSC_HOSTNAME" ]; then
+  export PUBLIC_BASE_URL="${PUBLIC_BASE_URL:-https://$OSC_HOSTNAME}"
+fi
+
+exec "$@"
